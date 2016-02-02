d4aaf4a4172896034ba2e458c9f59c90f1795d89
J Scray <jscray@Elanas-MacBook-Pro.local>
2016-02-02 11:33:52 -0800

    adding resources

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,6 +10,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/config.json', function(request, response) {
+  response.setHeader('Content-Type', 'application/json');
+  var config = JSON.parse(fs.readFileSync('views/partials/config.json', 'utf8'));
+  response.send(JSON.stringify(config));
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
@@ -16,5 +23,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
